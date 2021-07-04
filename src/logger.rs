f77e5ccc0f4b2e55a35440e50998220765903598
Thomas Gotwig <tgotwig@gmail.com>
2021-07-04 18:03:01 +0200

    📦 Change final logs

--- src/logger.rs
+++ src/logger.rs
@@ -4,8 +4,8 @@ pub fn print_end_status(child: Result<Child, Error>, file_format: String) {
     let res = child.unwrap().wait_with_output();
     println!("{:?}\n", res);
     if res.unwrap().status.success() {
-        println!("Successfully generated 'output.{}'! 😆🎞", file_format)
+        println!("✅ Successfully generated 'output.{}'! 😆🎞", file_format)
     } else {
-        println!("Something went wrong 😖")
+        println!("❌ Something went wrong 😖")
     }
 }
